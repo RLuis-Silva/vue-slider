06ff218a78eb9c513ef93fb1a11fd27b7459b02d
Luis <s.ros.lui@gmail.com>
2020-11-11 19:03:36 +0100

    add function + event

--- main.js
+++ main.js
@@ -11,20 +11,47 @@ const app = new Vue({
             './img/image2.jpg',
             './img/image3.jpg',
             './img/image4.jpg'
-        ]
-
-        
-        
+        ],
+        intervalloId: 0
+    },
+    created(){
+        console.log("created");
+        this.inizioLoop();
     },
 
     methods: {
         //la funzione passa all' immagine successiva
         nextPhoto(){
+            // incremento della foto di 1
             this.indexPhoto ++;
 
             if(this.indexPhoto > (this.photos.length - 1) ){
                 this.indexPhoto = 0;
             }
+        },
+
+        prevPhoto(){
+            // decremento la foto 
+            this.indexPhoto--;
+
+            if(this.indexPhoto < 0){
+                this.indexPhoto = this.photos.length - 1;
+            }
+        },
+
+        setPhoto(index){
+            this.indexPhoto = index;
+        },
+
+        inizioLoop(){
+            this.intervalloId = setInterval(()=>{
+                this.nextPhoto();
+            }, 3000);
+        },
+
+        // ferma il loop se il mouse è sopra lo slider
+        fermaLoop(){
+            clearInterval(this.intervalloId);
         }
     }
 });
